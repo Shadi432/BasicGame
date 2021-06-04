b00a23279c7c2552e1ef4950de033d7c277b8859
Mayz360 <85352463+Mayz360@users.noreply.github.com>
2021-06-04 15:58:43 +0100

    Add files via upload

--- PowerPong/main.lua
+++ PowerPong/main.lua
@@ -1,15 +1,16 @@
 
 
 function love.load()
-    print("Test")
+
+    
 end
 
 function love.update()
-    --[[
+
     for i=1, 10 do
         print(i)
     end
-    ]]--
+
 end
 
 function love.draw()
